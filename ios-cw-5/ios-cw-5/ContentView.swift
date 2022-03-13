baf99271f0835ecc44207fb7c5578268a2b68f78
Mohammed Feras Al Bahar <100701492+KamakiMoji@users.noreply.github.com>
2022-03-13 20:25:36 +0300

    done!

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,13 +9,78 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.red
+            VStack {
+                
+      
+                HStack {
+                
+                Image ("background")
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .top)
+                    .scaledToFit()
+                    .clipShape(Circle ())
+        
+                    Text("ios beats android!")
+                        .font(.title)
+                  
+                    
+                    
+                                } // hstack 1
+                    
+           
+               
+            
+                
+                HStack {
+                    Image ("image1")
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .topLeading)
+                        .scaledToFit()
+                        .clipShape(Rectangle())
+                    Image ("image2")
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .topLeading)
+                        .scaledToFit()
+                        .clipShape(Rectangle())
+                    Image ("image3")
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .topLeading)
+                        .scaledToFit()
+                        .clipShape(Rectangle())
+                }
+                HStack {
+                    Image ("image4")
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .topLeading)
+                        .scaledToFit()
+                        .clipShape(Rectangle())
+                    Image ("image5")
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .topLeading)
+                        .scaledToFit()
+                        .clipShape(Rectangle())
+                    Image ("image6")
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .topLeading)
+                        .scaledToFit()
+                        .clipShape(Rectangle())
+                    Spacer()
+                }
+                Spacer ()
+             
+            } .padding(50)
+        }.ignoresSafeArea()
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
